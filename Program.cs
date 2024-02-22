6f38b9dbe081cf3d4793dbf897bed18c1957b398
Saviodias14 <saviodiasviana@gmail.com>
2024-02-22 10:39:41 -0300

    Implementação da herança nas classes e testes no Program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new("991472152", "IPhone 12X", "78989538995754857", 256);
+Console.WriteLine(iphone.Numero);
+Console.WriteLine(iphone.Modelo);
+Console.WriteLine(iphone.IMEI);
+Console.WriteLine(iphone.Memoria);
+
+iphone.InstalarAplicativo("WhatsApp");
+Nokia nokia = new("992020599", "Nokia Tijolão", "93800934800398493", 32);
+Console.WriteLine(nokia.Numero);
+Console.WriteLine(nokia.Modelo);
+Console.WriteLine(nokia.IMEI);
+Console.WriteLine(nokia.Memoria);
+
+nokia.InstalarAplicativo("Jogo da cobrinha");
+
